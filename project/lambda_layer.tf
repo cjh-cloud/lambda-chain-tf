7ce7c2cb06187bc2e70044877360e16c80a3f268
cjh-cloud <clooneyjh@gmail.com>
2023-11-14 21:23:49 +1300

    feat: refactor layer

--- project/lambda_layer.tf
+++ project/lambda_layer.tf
@@ -1,8 +1,10 @@
 #define variables
 locals {
-  layer_zip_path    = "layer.zip"
+  layer_path        = "lambda_layer"
+  layer_zip_name    = "layer.zip"
   layer_name        = "lambda_layer_${var.environment}"
-  requirements_path = "${path.module}/lambda_layer/requirements.txt"
+  requirements_name = "requirements.txt"
+  requirements_path = "${path.module}/${local.layer_path}/${local.requirements_name}"
 }
 
 # create zip file from requirements.txt. Triggers only when the file is updated
@@ -13,10 +15,11 @@ resource "null_resource" "lambda_layer" {
   # the command to install python and dependencies to the machine and zips
   provisioner "local-exec" {
     command = <<EOT
+      cd ${local.layer_path}
       rm -rf python
       mkdir python
-      pip3 install -r ${local.requirements_path} -t python/
-      zip -r ${local.layer_zip_path} python/
+      pip3 install -r ${local.requirements_name} -t python/
+      zip -r ${local.layer_zip_name} python/
     EOT
   }
 }
@@ -29,8 +32,8 @@ resource "aws_s3_bucket" "lambda_layer" {
 # upload zip file to s3
 resource "aws_s3_object" "lambda_layer_zip" {
   bucket     = aws_s3_bucket.lambda_layer.id
-  key        = "lambda_layers/${local.layer_name}/${local.layer_zip_path}"
-  source     = local.layer_zip_path
+  key        = "lambda_layers/${local.layer_name}/${local.layer_zip_name}"
+  source     = "${local.layer_path}/${local.layer_zip_name}"
   depends_on = [null_resource.lambda_layer] # triggered only if the zip file is created
 }
 
